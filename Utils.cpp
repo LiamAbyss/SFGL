a969fbcb180f3d635f1ef5102a01f6c5468b9e0a
LiamAbyss <46683039+LiamAbyss@users.noreply.github.com>
2022-03-10 20:08:34 +0100

    Fix toUpper and toLower

--- Utils.cpp
+++ Utils.cpp
@@ -147,7 +147,7 @@ std::string sfg::utils::strings::toUpper(const std::string& str)
     std::stringstream upper;
     for (const auto& item : str)
     {
-        upper << toupper(item);
+        upper << (char)toupper(item);
     }
     return upper.str();
 }
@@ -157,7 +157,7 @@ std::string sfg::utils::strings::toLower(const std::string& str)
     std::stringstream lower;
     for (const auto& item : str)
     {
-        lower << tolower(item);
+        lower << (char)tolower(item);
     }
     return lower.str();
 }
@@ -167,9 +167,9 @@ std::string sfg::utils::strings::strip(const std::string& str)
     auto start_it = str.begin();
     auto end_it = str.rbegin();
 
-    while (std::isspace(*start_it) != 0)
+    while (std::isspace(*start_it))
         ++start_it;
-    while (std::isspace(*end_it) != 0)
+    while (std::isspace(*end_it))
         ++end_it;
 
     return std::string(start_it, end_it.base());
